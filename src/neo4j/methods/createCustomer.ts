0c784845e5ae8714c5c0f74625c1ddb437eb1aa4
ino <littletasteofheaven@protonmail.com>
2022-06-11 08:26:02 +0600

    Move logging inside method body

--- src/neo4j/methods/createCustomer.ts
+++ src/neo4j/methods/createCustomer.ts
@@ -1,10 +1,13 @@
 import {QueryResult} from "neo4j-driver"
 import {neo4jSession} from "../index"
 
-export function createCustomer(phoneNumber: string): Promise<QueryResult> {
-  return neo4jSession.run(
+export async function createCustomer(phoneNumber: string): Promise<QueryResult> {
+  const customer = await neo4jSession.run(
     "MERGE (c:Customer {phone: $phoneNumber}) RETURN c",
     {phoneNumber}
   )
+  console.log("Created customer:", customer.records[0].get(0))
+  console.log("Query summary:", customer.summary.counters, customer.summary.updateStatistics)
+  return customer
 }
 
