702329aedff4964fdf5992f2c583deec70d7ec67
ino <littletasteofheaven@protonmail.com>
2022-06-15 03:41:46 +0600

    Return the promisified token

--- src/jwt/signRsa.ts
+++ src/jwt/signRsa.ts
@@ -1,28 +1,28 @@
-import * as jwt from "jsonwebtoken"
-import {readFile} from "node:fs/promises"
-import {homedir} from "os"
+import jwt from "jsonwebtoken"
 import {SignOptions} from "jsonwebtoken";
 import {errDetails} from "../error/errDetails";
+import {keys} from "../keys";
 
 interface Options extends SignOptions {
   algorithm: "RS256"
 }
 
-export async function signJwtRsa(phoneNumber: string) {
-  const home = homedir()
-  const privateKeyPath = `${home}/.ssh/id_rsa`
-
-  // Parameters for jwt.sign()
-  const privateKey = await readFile(privateKeyPath, {encoding: "utf8"})
+export function signJwtRsa(phoneNumber: string) {
+  const secretOrPrivateKey = {
+    key: keys.privateKey.key,
+    passphrase: keys.privateKey.passphrase
+  }
   const payload = {phoneNumber}
   const options: Options = {algorithm: "RS256"}
-  const callback = (err: any, token: string) => {
-    if (err) {
-      console.error(errDetails(err))
-    } else {
-      console.info("JWT:", token)
-    }
-  }
 
-  jwt.sign(payload, privateKey, options, callback)
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, secretOrPrivateKey, options, (err: any, token: string) => {
+      if (err) {
+        console.error(errDetails(err))
+        reject(err)
+      } else {
+        resolve(token)
+      }
+    })
+  })
 }
