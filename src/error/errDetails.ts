b336b0fe44cda12dfcd4699be87eaf9dd864ef4a
ino <littletasteofheaven@protonmail.com>
2022-07-04 13:05:20 +0600

    Some minor change

--- src/error/errDetails.ts
+++ src/error/errDetails.ts
@@ -1,10 +1,28 @@
-export function errDetails(err: Error | unknown): string {
-  let errorDetails: string
+type ErrorT = {
+  name: string | null,
+  message: string,
+  cause: Error | null | undefined,
+  stack: string | null | undefined
+} | string
+
+export function errDetails(err: Error | unknown): ErrorT {
+  let errorDetails: ErrorT = {
+    name : null,
+    message: "",
+    cause: null,
+    stack: null
+  }
 
   if (err instanceof Error) {
-    errorDetails = err.message
+    errorDetails.name = err.name
+    errorDetails.message = err.message
+    errorDetails.cause = err.cause
+    errorDetails.stack = err.stack
   } else {
-    errorDetails = err as string
+    errorDetails.name = null
+    errorDetails.message = err as string
+    errorDetails.cause = null
+    errorDetails.stack = null
   }
 
   return errorDetails
