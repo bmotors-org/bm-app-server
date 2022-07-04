7a45adf953a30c08980a8f07bd85e6cc27f60404
ino <littletasteofheaven@protonmail.com>
2022-07-04 13:04:37 +0600

    Add profile routes, test neo4j and redis connectivity

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,18 @@ import express from "express"
 import morgan from "morgan"
 import helmet from "helmet";
 import {baseFragment, router} from "../routes"
+import "../redis"
+import "../neo4j"
 
 export const app = express()
+
+app.set("trust proxy", true)
 
 app
   .use(helmet())
   .use(express.json())
   .use(`${baseFragment}/auth`, router.authRoutes)
+  .use(`${baseFragment}/profile`, router.profileRoutes)
   .use(morgan("tiny"))
 
 
