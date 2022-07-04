de99c5009aaf4e7395a375498d4a4be2846a8a10
ino <littletasteofheaven@protonmail.com>
2022-07-05 02:32:57 +0600

    Misc

--- src/neo4j/methods/mergeCustomer.ts
+++ src/neo4j/methods/mergeCustomer.ts
@@ -1,6 +1,11 @@
 import {neo4jSession} from "../index"
 
-export async function mergeCustomer(phoneNumber: string) {
+type UserData = {
+  name: string,
+  email: string
+}
+
+export async function mergeCustomer(phoneNumber: string): Promise<UserData> {
   const customer = await neo4jSession.run(
     `
     MERGE (c:Customer {phoneNumber: $phoneNumber})
@@ -11,5 +16,12 @@ export async function mergeCustomer(phoneNumber: string) {
   )
   console.log("Created customer:", customer.records[0].get(0))
   console.log("Query summary:", customer.summary.counters, customer.summary.updateStatistics)
+
+  const userData: UserData = {name: "", email: ""}
+
+  userData.name = customer.records[0].get(0).properties.name || ""
+  userData.email = customer.records[0].get(0).properties.email || ""
+
+  return userData
 }
 
