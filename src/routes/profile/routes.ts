466dd209540054dcf1b2c813a99b4fe65ba878ce
ino <littletasteofheaven@protonmail.com>
2022-07-05 02:33:45 +0600

    Add a `merge-email` route

--- src/routes/profile/routes.ts
+++ src/routes/profile/routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import {MergeNameRequest} from "./interfaces";
+import {MergeEmailRequest, MergeNameRequest} from "./interfaces";
 import {errDetails} from "../../error/errDetails";
 import {mergeName} from "../../neo4j/methods/mergeName";
 import {redisSessionStore} from "../../redis";
+import {mergeEmail} from "../../neo4j/methods/mergeEmail";
 
 const router = express.Router()
 
@@ -17,11 +18,31 @@ router.post("/merge-name", async (req: MergeNameRequest, res) => {
     const phoneNumber = await redisSessionStore.json.get(authHeader, {
       path: ".phoneNumber"
     })
-    console.log("phone number is", phoneNumber, "type:", typeof phoneNumber)
+    console.log("redis phone number is", phoneNumber)
     await mergeName(name, phoneNumber.toString())
     res.sendStatus(200)
   } catch (err) {
     console.log(errDetails(err))
+    res.sendStatus(500)
+  }
+})
+
+router.post("/merge-email", async (req: MergeEmailRequest, res) => {
+  const {email} = req.body
+
+  const authHeader = req.get("Authorization")
+
+  console.log("auth header is", authHeader)
+
+  try {
+    const phoneNumber = await redisSessionStore.json.get(authHeader, {
+      path: ".phoneNumber"
+    })
+    console.log("redis phone number is", phoneNumber)
+    await mergeEmail(email, phoneNumber.toString())
+    res.sendStatus(200)
+  } catch (err) {
+    console.error(errDetails(err))
     res.sendStatus(500)
   }
 })
